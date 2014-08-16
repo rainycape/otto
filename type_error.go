e9ccfb3503cbd32b0710d2924b3053b0a4df448b
Alberto García Hierro <alberto@garciahierro.com>
2014-08-16 03:11:54 +0200

    Escape % signs when creating an error object
    Otherwise, the error message will end up corrupted if it contains
    a % symbol because it's passed to fmt.Sprintf.

--- type_error.go
+++ type_error.go
@@ -1,11 +1,13 @@
 package otto
 
+import "strings"
+
 func (rt *_runtime) newErrorObject(name string, message Value) *_object {
 	self := rt.newClassObject("Error")
 	if message.IsDefined() {
 		msg := message.string()
 		self.defineProperty("message", toValue_string(msg), 0111, false)
-		self.value = newError(rt, name, msg)
+		self.value = newError(rt, name, strings.Replace(msg, "%", "%%", -1))
 	} else {
 		self.value = newError(rt, name)
 	}
